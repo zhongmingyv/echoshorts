0e3d8bde0a22a6acd0ef2558ee9c4b69c0ff26cd
zhongmingyu <zhongmingyu@qq.com>
2026-06-11 14:53:44 +0800

    billiard

--- lang-query.js
+++ lang-query.js
@@ -1,6 +1,6 @@
 /**
  * EchoShorts static pages: read ?lang=BCP47 from URL and open the matching locale file.
- * Used by home / billiard HTML. (index.html is a redirect; hotel.html handles ?lang= itself.)
+ * Used by home HTML. (index.html is a redirect; hotel.html and billiard.html handle ?lang= themselves.)
  */
 (function () {
   try {
@@ -25,25 +25,8 @@
       'th-TH': 'home-th.html',
       'vi-VN': 'home-vi.html',
     };
-    var BILLIARD = {
-      'en-US': 'billiard.html',
-      'zh-CN': 'billiard-zh-CN.html',
-      'zh-TW': 'billiard-zh-TW.html',
-      'de-DE': 'billiard-de-DE.html',
-      'es-ES': 'billiard-es-ES.html',
-      'fr-FR': 'billiard-fr-FR.html',
-      'id-ID': 'billiard-id-ID.html',
-      'ja-JP': 'billiard-ja-JP.html',
-      'ko-KR': 'billiard-ko-KR.html',
-      'pt-BR': 'billiard-pt-BR.html',
-      'ru-RU': 'billiard-ru-RU.html',
-      'th-TH': 'billiard-th-TH.html',
-      'vi-VN': 'billiard-vi-VN.html',
-    };
-
     var map = null;
     if (/^home/i.test(file)) map = HOME;
-    else if (/^billiard/i.test(file)) map = BILLIARD;
     if (!map) return;
 
     var target = map[want];
